f6b19f1725dff84f41cd76bf4ed02293f10f8fcd
anirudh242 <anirudhbhardwaj48@gmail.com>
2025-09-12 18:08:41 +0530

    pieces falling and rotation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <windows.h> 
+#include <random>
+#include <conio.h>   
+#include <cstdlib>
+
+
 
 const int WIDTH = 12, HEIGHT = 21;
 std::vector<std::vector<int>> board(HEIGHT, std::vector<int>(WIDTH, 0));
@@ -62,6 +67,20 @@ int currentRotation = 0;
 int currentX = (WIDTH / 2) - 1; // center
 int currentY = 0;
 
+int getRotatedBlock(int piece, int rotation, int row, int col) {
+    int r = row;
+    int c = col;
+
+    for (int i = 0; i < rotation % 4; ++i) {
+        // transpose + reverse to rotate
+        int temp = r;
+        r = c;
+        c = 3 - temp;
+    }
+
+    return pieces[piece][r][c];
+}
+
 void clearScreen()
 {
     system("clear");
@@ -87,10 +106,14 @@ void initBoard()
 
 void drawBoard()
 {
-     for (int i = 0; i < 4; ++i) {
+    std::vector<std::vector<int>> displayBoard = board; // temporary to display before locking
+
+
+     
+    for (int i = 0; i < 4; ++i) {
         for (int j = 0; j < 4; ++j) {
-            if (pieces[currentPiece][i][j] == 1) {
-                board[currentY + i][currentX + j] = 1; 
+            if (getRotatedBlock(currentPiece, currentRotation, i, j) == 1) {
+                displayBoard[currentY + i][currentX + j] = 1; 
             }
         }
     }
@@ -99,7 +122,7 @@ void drawBoard()
     {
         for (int j = 0; j < WIDTH; j++)
         {
-            switch (board[i][j]) {
+            switch (displayBoard[i][j]) {
                 case 0:
                     std::cout << " .";
                     break;
@@ -115,13 +138,101 @@ void drawBoard()
     }
 }
 
+bool doesPieceFit(int  piece, int rot, int xPos, int yPos)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (getRotatedBlock(piece, rot, i, j)  == 1) {
+                if (board[yPos + i][xPos + j] != 0) {
+                    return false;
+                }
+            }
+        } 
+    }
+    return true;
+}
+
 int main(int argc, char const *argv[])
 {
+    // random number generation
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(0, 6);
+    currentPiece = distrib(gen);
+
     initBoard();
     
     bool isGameOver = false;
-    while (!isGameOver) {
-        Sleep(50);
+    int tickCounter = 10;
+    int gameSpeed = 20;
+
+    while (!isGameOver) 
+    {
+        Sleep(100);
+        tickCounter++;
+
+        char input = 0;
+        if (_kbhit()) {
+            input = _getch();
+        }
+
+        if (input == 'a' || input == 'A') { // Left
+            if (doesPieceFit(currentPiece, currentRotation, currentX - 1, currentY)) 
+            {
+                currentX--;
+            }
+        }
+        if (input == 'd' || input == 'D') { // Right
+            if (doesPieceFit(currentPiece, currentRotation, currentX + 1, currentY))
+             {
+                currentX++;
+            }
+        }
+        if (input == 's' || input == 'S') 
+        { // Down (Soft Drop)
+            if (doesPieceFit(currentPiece, currentRotation, currentX, currentY + 1)) 
+            {
+                currentY++;
+            }
+        }
+        if (input == 'w' || input == 'W') 
+        { // Rotate
+            int nextRotation = (currentRotation + 1) % 4;
+            if (doesPieceFit(currentPiece, nextRotation, currentX, currentY)) 
+            {
+                currentRotation = nextRotation;
+            }
+        }
+
+        if (tickCounter >= gameSpeed)
+        {
+            if (doesPieceFit(currentPiece, currentRotation, currentX, currentY + 1))
+            {
+                currentY++;
+            } 
+            else
+            {
+                // lock
+                for (int i = 0; i < 4; ++i) {
+                    for (int j = 0; j < 4; ++j) {
+                        if (getRotatedBlock(currentPiece, currentRotation, i, j) == 1) {
+                            board[currentY + i][currentX + j] = 1;
+                        }
+                    }
+                }
+
+                currentRotation = 0;\
+                currentX = (WIDTH / 2) - 2;
+                currentY = 0;
+                currentPiece = distrib(gen);
+
+                if (!doesPieceFit(currentPiece, currentRotation, currentX, currentY)) {
+                    isGameOver = true;
+                }
+            }
+        }
         clearScreen();
         drawBoard();
     }
