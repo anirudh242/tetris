71ad6acc326990afa731d11faccda15d188974ce
anirudh242 <anirudhbhardwaj48@gmail.com>
2025-09-12 17:33:24 +0530

    added pieces

--- main.cpp
+++ main.cpp
@@ -1,8 +1,66 @@
 #include <iostream>
 #include <vector>
+#include <windows.h> 
 
-const int WIDTH = 11, HEIGHT = 21;
+const int WIDTH = 12, HEIGHT = 21;
 std::vector<std::vector<int>> board(HEIGHT, std::vector<int>(WIDTH, 0));
+
+std::vector<std::vector<std::vector<int>>> pieces = {
+    // I piece
+    {
+        {0,0,0,0}, 
+        {1,1,1,1}, 
+        {0,0,0,0}, 
+        {0,0,0,0}
+    },
+    // O piece
+    {
+        {0,0,0,0}, 
+        {0,1,1,0}, 
+        {0,1,1,0}, 
+        {0,0,0,0}
+    },
+    // T piece
+    {
+        {0,0,0,0},
+        {0,1,0,0},
+        {1,1,1,0}, 
+        {0,0,0,0}
+    },
+    // L piece
+    {
+        {0,0,0,0}, 
+        {0,0,1,0}, 
+        {1,1,1,0}, 
+        {0,0,0,0}
+    },
+    // J piece
+    {
+        {0,0,0,0}, 
+        {1,0,0,0}, 
+        {1,1,1,0}, 
+        {0,0,0,0}
+    },
+    // Z piece
+    {
+        {0,0,0,0}, 
+        {1,1,0,0}, 
+        {0,1,1,0}, 
+        {0,0,0,0}
+    },
+    // S piece
+    {
+        {0,0,0,0},
+        {0,1,1,0}, 
+        {1,1,0,0}, 
+        {0,0,0,0}
+    }
+};
+
+int currentPiece = 0;
+int currentRotation = 0;
+int currentX = (WIDTH / 2) - 1; // center
+int currentY = 0;
 
 void clearScreen()
 {
@@ -29,17 +87,28 @@ void initBoard()
 
 void drawBoard()
 {
+     for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            if (pieces[currentPiece][i][j] == 1) {
+                board[currentY + i][currentX + j] = 1; 
+            }
+        }
+    }
+
     for (int i = 0; i < HEIGHT; i++)
     {
         for (int j = 0; j < WIDTH; j++)
         {
-            if (board[i][j] == 9)
-            {
-                std::cout << " #"; 
-            }
-            else
-            {
-                std::cout << " ."; 
+            switch (board[i][j]) {
+                case 0:
+                    std::cout << " .";
+                    break;
+                case 9:
+                    std::cout << " #";
+                    break;
+                default:
+                    std::cout << "[]";
+                    break;
             }
         }
         std::cout << std::endl;
@@ -49,7 +118,14 @@ void drawBoard()
 int main(int argc, char const *argv[])
 {
     initBoard();
-    drawBoard();
+    
+    bool isGameOver = false;
+    while (!isGameOver) {
+        Sleep(50);
+        clearScreen();
+        drawBoard();
+    }
 
+    std::cout << "Game over!" << std::endl;
     return 0;
 }
